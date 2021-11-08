c3c525ebe1c53ce7c532afccc98b98d4f0db7c98
Oscar Lucas Garcia Lorenzo <oscarlgarcialorenzo@gmail.com>
2021-11-08 11:40:29 +0100

    cambios menu hamburguesa

--- docs/js/main.js
+++ docs/js/main.js
@@ -7,12 +7,12 @@ function menu() {
     var headerContenedor = document.getElementById("headerContenedor");
 
     if (toggle === "false") {
-        divMenu.style.background = "url(../images/icons/close-white-48dp.svg)";
+        divMenu.style.backgroundImage = "url(../images/icons/close-white-48dp.svg)";
         headerContenedor.style.height = "inherit";
         toggle = "true";
 
     } else {
-        divMenu.style.background = "url(../images/icons/menu-white-48dp.svg)";
+        divMenu.style.backgroundImage = "url(../images/icons/menu-white-48dp.svg)";
         headerContenedor.style.height = "75px";
         toggle = "false";
     }
