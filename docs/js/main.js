8435c1fcfcaf1c2969b37292ca2cd0c95c71f724
Oscar Lucas Garcia Lorenzo <oscarlgarcialorenzo@gmail.com>
2021-11-08 13:12:03 +0100

    cambios en icono menu hamburguesa

--- docs/js/main.js
+++ docs/js/main.js
@@ -18,3 +18,20 @@ function menu() {
     }
 }
 
+function menuLandpage() {
+
+    var divMenu = document.getElementById("menuLandpage");
+
+    var headerContenedor = document.getElementById("headerContenedor");
+
+    if (toggle === "false") {
+        divMenu.style.backgroundImage = "url(images/icons/close-white-48dp.svg)";
+        headerContenedor.style.height = "inherit";
+        toggle = "true";
+
+    } else {
+        divMenu.style.backgroundImage = "url(images/icons/menu-white-48dp.svg)";
+        headerContenedor.style.height = "75px";
+        toggle = "false";
+    }
+}
